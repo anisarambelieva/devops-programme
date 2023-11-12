ef274d54882ab615d440caa39e4b69865a6fcc7d
Anna Sarambelieva <ansa@milestone.dk>
2023-11-12 11:40:59 +0000

    Introduce error to test linters

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@ import os
 
 from flask import Flask
 
-app = Flask(__name__)
+App = Flask(__name__)
 
 
-@app.route("/")
+@App.route("/")
 def hello_world():
     return "Hello, cats!"
 
 
 if __name__ == "__main__":
-    app.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")
+    App.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")
